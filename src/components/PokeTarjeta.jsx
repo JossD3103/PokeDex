87323fe5bc4a9dfa28b7594fbda7fa18d7670b01
Josue31 <dirzojosue31@gmail.com>
2024-06-24 13:26:40 -0600

    Changes

--- src/components/PokeTarjeta.jsx
+++ src/components/PokeTarjeta.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+import axios from 'axios'
+import { 
+  Col, 
+  Card, 
+  CardBody, 
+  CardFooter, 
+  CardImg,
+  Badge,
+ } from 'reactstrap'
 
-const PokeTarjeta = () => {
+const PokeTarjeta = (params) => {
+  const [pokemon,setPokemon] = useState([]);
+  const [imagen, setImagen] = useState('');
+  const [cardClass, setCardClass] = useState(' d-none');
+  const [loadClass, setLoadClass] = useState('');
+
+  useEffect(() => {
+    getPokemon()
+  },[])
+
+  const getPokemon = async(o) => {
+    const apiPocket = params.poke.url;
+    axios.get(apiPocket).then( async(response) => {
+      const respuesta = response.data;
+      setPokemon(respuesta);
+      setCardClass('');
+      setLoadClass(' d-none');
+    })
+  }
   return (
-    <div>PokeTarjeta</div>
+    <Col sm='4' lg='3' className='mb-3'>
+      <Card className={'shadow-lg border-5 border-violet-400' + loadClass}>
+        <CardImg src='/public/img/loading2.gif' className="p-3"/>
+      </Card>
+      <Card className={'shadow-lg border-5 border-violet-400' + cardClass}>
+        <CardImg src={imagen} height='150' className='p-2'/>
+        <CardBody className="text-center">
+          <Badge pill color='danger'># {pokemon.id}</Badge>
+          <label className="uppercase text-base">{pokemon.name}</label>
+        </CardBody>
+        <CardFooter className="bg-violet-400">
+          <a className="btn bg-slate-800 text-white "><i className="fa-solid fa-arrow-up-right-from-square"></i> Detalle</a>
+        </CardFooter>
+      </Card>
+    </Col>
   )
 }
 
